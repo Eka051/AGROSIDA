48c09f070961cae37c53ec9d963a5da4f49ed469
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2023-10-30 16:43:06 +0700

    penambahan fitur

--- program.py
+++ program.py
@@ -6,10 +6,10 @@ import login
 def clear():
     os.system('cls')
 
-def back():
-    back = input("Klik \"ENTER\" Untuk Kembali")
-    main_menu()
-    print(back)
+# def back():
+#     back = input("Klik \"ENTER\" Untuk Kembali")
+#     main_menu()
+#     print(back)
     
 def main_menu():
     clear()
@@ -18,38 +18,71 @@ def main_menu():
     |{"A G R O S I D A".center(58)}|
     |{"Hama Hilang, Petani Aman".center(58)}|
     {'='*60}
-    |{"[1]. Informasi Tanaman dan Pestisida".ljust(58)}|
-    |{"[2]. Tambah Stok Pestisida".ljust(58)}|
-    |{"[3]. Hapus Data Pestisida".ljust(58)}|
-    |{"[4]. Tambah Transaksi".ljust(58)}|
-    |{"[5]. Informasi Penjualan".ljust(58)}|
+    |{"[1]. Informasi Tanaman".ljust(58)}|
+    |{"[2]. Pestisida".ljust(58)}|
+    |{"[3]. Rekap Penjualan".ljust(58)}|
     {'='*60}
     ''')
     option = int(input("Masukkan Nomor Untuk Memilih Opsi: "))
     if option == 1:
-        info_1 = open("info_tanaman.txt")
-        info_2 = info_1.readlines()[0:40]
-        for a in info_2:
-            print(a)
-    # elif option == 2:
-    #     add_stock():
-    # elif option == 3:
-    #     delete_stock()
-    # elif option == 4:
-    #     add_transaction()
-    # elif option == 5:
-    #     sales_info()
-    # else:
-    #     print("Masukkan Pilihan Yang Benar")
-    #     back()
+        info_tanaman()
+    elif option == 2:
+        menu_pestisida()
+    elif option == 3:
+        rekap_penjualan()
+    else:
+        input("Masukkan Pilihan Yang Benar. Klik ENTER untuk kemabali")
+        main_menu()
+    
+main_menu()   
+def info_tanaman():
+    clear()
+    print(f'''
+    {"="*60}
+    |{"A G R O S I D A".center(58)}|
+    |{"Hama Hilang, Petani Aman".center(58)}|
+    {'='*60}
+    |{"[1]. Tampilkan Informasi Tanaman".ljust(58)}|
+    |{"[2]. Entri Informasi Tanaman".ljust(58)}|
+    |{"[3]. Edit Informasi Tanaman".ljust(58)}|
+    |{"[4]. Update Informasi Tanaman".ljust(58)}|
+    |{"[5]. Hapus Informasi Tanaman".ljust(58)}|
+    {'='*60}
+    ''')
+    option = int(input("Masukkan Pilihan: "))
+    
+    if option == 1:
+        show_data()
+    elif option == 2:
+        entri_data()
+    elif option == 3:
+        edit_data()
+    elif option == 4:
+        update_data()
+    elif option == 5:
+        delete_data()
+    else:
+        input("Masukkan Pilihan Yang Benar. Klik ENTER untuk kembali")
+        info_tanaman()
+    
+def info_tanaman():
+    clear()
+    print(f'''
+    {"="*60}
+    |{"A G R O S I D A".center(58)}|
+    |{"Hama Hilang, Petani Aman".center(58)}|
+    {'='*60}
+    |{"[1]. Tambah Transaksi Pestisida".ljust(58)}|
+    |{"[2]. Tampilkan Transaksi".ljust(58)}|
+    {'='*60}
+    ''')
+    option = int(input("Masukkan Pilihan: "))
+    
+def rekap_penjualan():
+    clear()
     
     
-# def info():
-#     clear()
-#     list_tanaman = []
-#     with open("info_tanaman.txt", mode="r") as info_tanaman:
-#         reader = csv.DictReader(info_tanaman)
-#         for row in reader:
-#             list_tanaman.append(row)
-
+    
+    
+    
 main_menu()
